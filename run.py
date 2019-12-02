843625abc720101794f9fdc6e1c4b6e8cd9a8ee4
romqrq <rjaalbuquerque@gmail.com>
2019-12-02 14:28:35 +0000

    Fixed run.py and added requirements

--- run.py
+++ run.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello There!</h1>"
 
+# This is the way the lesson showed but it VS throws an error saying port has to be a str instead of int
+# app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("IP"),
+            port=(os.getenv("PORT")),
+            debug=True)
