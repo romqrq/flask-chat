f2c199e591338dd54892668941b7d3a0b6535819
romqrq <rjaalbuquerque@gmail.com>
2019-12-02 15:13:20 +0000

    add the ability yo write and to read from a list and display the messages to the user

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+messages = []
+
+
+def add_messages(username, message):
+    """Add messages to the messages list"""
+    messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Get all the messages and separate them with a 'br'"""
+    return "<br>".join(messages)
 
 
 @app.route('/')
@@ -12,12 +24,15 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return 'Hi ' + username
+    """ Display chat messages """
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return '{0}: {1}'.format(username, message)
+    """ Create a new message and redurect back to chat page """
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 # This is the way the lesson showed but it VS throws an error saying port has to be a str instead of int
